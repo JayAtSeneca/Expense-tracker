85fc4f2835c4b8fc379163e6e2fa1df00053e55b
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-04-13 15:21:50 -0400

    update passport config added google login strategy

--- config/passport.js
+++ config/passport.js
@@ -78,6 +78,43 @@ module.exports = app => {
     )
   )
 
+  // Set up google strategy
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK
+      },
+      async (token, tokenSecret, profile, done) => {
+        const { sub, name, email, picture } = profile._json
+        try {
+          // User's googleId or email must be unique
+          let user = await User.findOne({
+            $or: [{ email }, { googleId: sub }]
+          })
+          if (!user) {
+            const randomPassword = Math.random().toString(36).slice(-8)
+
+            user = await User.create({
+              name,
+              email,
+              avatar: picture || `https://robohash.org/${name}`,
+              password: bcrypt.hashSync(
+                randomPassword,
+                bcrypt.genSaltSync(10),
+                null
+              ),
+              googleId: sub
+            })
+          }
+          return done(null, user)
+        } catch (err) {
+          done(err)
+        }
+      }
+    )
+  )
 
   passport.serializeUser((user, done) => {
     done(null, user.id)
