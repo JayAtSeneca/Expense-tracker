3f7efc0481977e8ddbf870a619a1df7eaea6ec71
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-04-15 22:39:42 -0400

    update auth.js added the facebook authenticate

---
+++ routes/modules/auth.js
@@ -0,0 +1,17 @@
+// Require Express and Express router
+const express = require('express')
+const router = express.Router()
+
+const userController = require('../../controllers/userController')
+const { authenticator } = require('../../middleware/auth')
+
+const passport = require('passport')
+
+// Define routes
+// Facebook
+router.get(
+  '/facebook',
+  passport.authenticate('facebook', {
+    scope: ['email', 'public_profile']
+  })
+)
